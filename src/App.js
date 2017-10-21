3e15f25cef2f0628a8fac31dfb3052f4500a475c
Aidan <calopter@gmail.com>
2017-10-21 15:34:29 -0400

    fixed tooling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import * as d3 from 'd3'
+import './App.css'
 
-class App extends Component {
-  render() {
+const TreeView = function(treeData) {
+    const makeTree = d3.tree(); //.size([1000, 1000]);
+    const root = d3.hierarchy(treeData);
+    const treeLayout = makeTree(root);
+    const nodes = treeLayout.descendants();
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div className="tree-container">
+          {nodes.map((node, i) =>
+            <Card key={i} 
+              style={{transform: `translate(${node.x}px, ${node.y}px)`}}
+              {...node} />
+          )}
+        </div>
     );
-  }
 }
 
-export default App;
+const Card = ({type, title, mood}) =>
+    <section className={type}>
+        <p className="title">{title}</p>
+        <div className="mood">{mood}</div>
+        <button>remove</button>
+    </section>
+
+const root = {
+    type: "period-card",
+    title: "ecce app",
+    mood: "light"
+}
+
+const init = [
+  {
+    type: "period-card",
+    title: "node1",
+    mood: "light"
+  },
+  {
+    type: "period-card",
+    title: "node2",
+    mood: "light"
+  },
+  {
+    type: "period-card",
+    title: "node3",
+    mood: "light"
+  }
+]
+
+const initTree = {
+  cards: [root],
+  children: [
+    {
+      cards: init,
+      children: []
+    }
+  ]
+}
+
+const App = () =>
+    <div className="app">
+        <Card {...root} />
+    </div>
+
+export default App
