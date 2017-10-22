943620d53e7cc012e0e1635c348228c48aae97ef
Aidan <calopter@gmail.com>
2017-10-21 21:50:09 -0400

    connect redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,6 @@
-import React from 'react'
-import { createStore } from 'redux'
-import { v4 } from 'uuid'
-import { cards } from '../reducers'
-import A from '../actionTypes'
-import CardTree from './CardTree.js'
-import initTree from '../resources/initTree.json'
-import '../stylesheets/App.css'
+import { connect } from 'react-redux'
+import CardTree from './CardTree'
 
-const store = createStore(cards, initTree)
-
-console.log(store.getState())
-
-const newCard = {
-  id: v4(),
-  type: 'period',
-  mood: 'dark',
-  name: 'newkid',
-  children: Array(0)
-}
-
-const newCardAction = {
-  type: A.ADD_CARD,
-  targId: "7d8d8aa9-8dbf-4f83-b576-43c89d77d8b4",
-  card: newCard
-}
-
-store.dispatch(newCardAction)
-console.log(store.getState())
-
-const removeCardAction = {
-  type: A.REMOVE_CARD,
-  card: newCard
-}
-
-store.dispatch(removeCardAction)
-console.log(store.getState())
-
-const App = () =>
-    <div className="app">
-        <CardTree {...initTree} />
-    </div>
+const App = connect(state => state)(CardTree)
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './stylesheets/index.css';
-import App from './components/App.js';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import { v4 } from 'uuid'
+
+import App from './components/App'
+import initTree from './resources/initTree.json'
+import { cards } from './reducers'
+import A from './actionTypes'
+
+import './stylesheets/index.css'
+
+const store = createStore(cards, initTree)
+
+console.log(store.getState())
+
+const newCard = {
+  id: v4(),
+  type: 'period-card',
+  mood: 'dark',
+  title: 'newkid',
+  children: Array(0)
+}
+
+const newCardAction = {
+  type: A.ADD_CARD,
+  targId: "7d8d8aa9-8dbf-4f83-b576-43c89d77d8b4",
+  card: newCard
+}
+
+store.dispatch(newCardAction)
+console.log(store.getState())
+
+const removeCardAction = {
+  type: A.REMOVE_CARD,
+  card: newCard
+}
+
+//store.dispatch(removeCardAction)
+//console.log(store.getState())
+
+render(
+  <Provider store={store}>
+    <App/>
+  </Provider>, 
+  document.getElementById('root')
+)
+
+registerServiceWorker()
